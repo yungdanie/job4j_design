98dd37757f5de99ba2004ba71b32ae54b1996d2e
yungdanie <nikoches253@gmail.com>
2022-04-15 18:00:11 +0300

    6. Перевернуть связанный список [#161 #289369]

--- src/test/java/ru/job4j/iterator/ListUtilsTest.java
+++ src/test/java/ru/job4j/iterator/ListUtilsTest.java
@@ -14,28 +14,22 @@ public class ListUtilsTest {
 
     @Test
     public void whenRemoveIf() {
-        List<Integer> input = new ArrayList<>(Arrays.asList(1, 3));
-        removeIf(input, x -> x.equals((Integer) 1));
+
     }
 
     @Test
     public void whenAddBefore() {
-        List<Integer> input = new ArrayList<>(Arrays.asList(1, 3));
-        ListUtils.addBefore(input, 1, 2);
-        assertThat(input, is(Arrays.asList(1, 2, 3)));
+
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void whenAddBeforeWithInvalidIndex() {
-        List<Integer> input = new ArrayList<>(Arrays.asList(1, 3));
-        ListUtils.addBefore(input, 3, 2);
+
     }
 
     @Test
     public void whenAddAfterLast() {
-        List<Integer> input = new ArrayList<>(Arrays.asList(0, 1, 2));
-        ListUtils.addAfter(input, 2, 3);
-        assertThat(input, is(Arrays.asList(0, 1, 2, 3)));
+
     }
 
 }
