bee6191c903612f6f63335df503a28e9421011e1
yungdanie <nikoches253@gmail.com>
2022-04-27 15:28:43 +0300

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,46 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        String temp;
+       try (BufferedReader in = new BufferedReader(new FileReader(path))) {
+           while (in.ready()) {
+               temp = in.readLine();
+               if (!temp.startsWith("#") && !temp.isEmpty()) {
+                   String[] array = temp.split("=");
+                   if (array.length == 1 || array.length > 2) {
+                       throw new IllegalArgumentException();
+                   }
+                   values.put(array[0], array[1]);
+               }
+           }
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    public static void main(String[] args) {
+        Config config = new Config("./app.properties");
+        config.load();
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.io;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class ConfigTest {
+
+    @Test
+    public void whenSuccessfulLoadAndGetFileWithCommAndSpace() {
+        Config config = new Config("./data/prop_with_comments_and_space.properties");
+        config.load();
+        assertThat(config.value("gorod"), is("spb"));
+        assertThat(config.value("daniel"), is("kocheshkov"));
+    }
+
+    @Test
+    public void whenSuccessfulLoadAndGetFileWithoutComm() {
+        Config config = new Config("./data/prop_without_comments.properties");
+        config.load();
+        assertThat(config.value("gorod"), is("spb"));
+        assertThat(config.value("daniel"), is("kocheshkov"));
+        assertThat(config.value("univer"), is("gorniy"));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenLoadAndIllegalArgumentExc() {
+        Config config = new Config("./data/prop_with_illegal_pattern.properties");
+        config.load();
+        assertThat(config.value("kocheshkov"), is("daniel"));
+    }
+}
