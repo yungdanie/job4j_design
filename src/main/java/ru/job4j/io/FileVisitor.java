ee7aa77d23b5e39c914493588624354f2cb204ba
yungdanie <nikoches253@gmail.com>
2022-04-28 13:32:00 +0300

    4.1. Сканирование файловой системы. [#106929]

---
+++ src/main/java/ru/job4j/io/FileVisitor.java
@@ -0,0 +1,30 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+public class FileVisitor extends SimpleFileVisitor<Path> {
+    private final Predicate<Path> condition;
+    private ArrayList<Path> list = new ArrayList<>();
+
+    public FileVisitor(Predicate<Path> condition) {
+        this.condition = condition;
+    }
+
+    public ArrayList<Path> getPaths() {
+        return list;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (condition.test(file)) {
+            list.add(file.getFileName());
+        }
+        return FileVisitResult.CONTINUE;
+    }
+}
---
+++ src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,18 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Search {
+
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        FileVisitor searcher = new FileVisitor(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+
+}
