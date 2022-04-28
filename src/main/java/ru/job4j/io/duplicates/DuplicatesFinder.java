9f5dd61fe43346bb043d85f15e0c745fc8574a37
yungdanie <nikoches253@gmail.com>
2022-04-28 18:47:26 +0300

    4.2. Поиск дубликатов [#315066 #294794]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -6,6 +6,8 @@ import java.nio.file.Path;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        Files.walkFileTree(Path.of("C:/projects/musor"), new DuplicatesVisitor());
+        DuplicatesVisitor duplicatesVisitor = new DuplicatesVisitor();
+        Files.walkFileTree(Path.of("C:/musor"), duplicatesVisitor);
+        duplicatesVisitor.showDuplicates();
     }
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -1,24 +1,41 @@
 package ru.job4j.io.duplicates;
 
-import java.io.IOException;
 import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
     private HashMap<FileProperty, Path> map = new HashMap<>();
 
+    public HashMap<FileProperty, HashSet<Path>> mapSet = new HashMap<>();
+
+    public void showDuplicates() {
+        if (mapSet != null) {
+            for (HashSet<Path> paths : mapSet.values()) {
+                paths.stream().iterator().forEachRemaining(System.out::println);
+            }
+        }
+    }
+
     @Override
-    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
         Path path;
         FileProperty fileProperty = new FileProperty(attrs.size(), file.getFileName().toString());
         path = map.putIfAbsent(fileProperty, file);
         if (path != null) {
-            Files.delete(file);
+            HashSet<Path> temp = mapSet.get(fileProperty);
+            if (temp == null) {
+                mapSet.put(fileProperty, new HashSet<>());
+                temp = mapSet.get(fileProperty);
+                temp.add(file);
+                temp.add(path);
+            } else {
+                temp.add(file);
+            }
         }
         return FileVisitResult.CONTINUE;
     }
