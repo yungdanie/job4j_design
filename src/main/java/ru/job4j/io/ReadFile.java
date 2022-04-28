53981598a44bf8e88b9ed3a4eb2209b6bcc91ebf
yungdanie <nikoches253@gmail.com>
2022-04-28 15:41:26 +0300

    recover

---
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -0,0 +1,15 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        try (BufferedReader in = new BufferedReader(new FileReader("input.txt"))) {
+            in.lines().forEach(System.out::println);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/test/TestMladshiy.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-public class TestMladshiy extends Test {
-
-    public void println() {
-        System.out.println("Mladshiy");
-    }
-}
