ccf525980e70924977869b4f0d228e870bd0a350
yungdanie <nikoches253@gmail.com>
2022-05-03 15:49:47 +0300

    6. Кодировка. [#862]

---
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -0,0 +1,86 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class ConsoleChat {
+    private static final String OUT = "закончить";
+    private static final String STOP = "стоп";
+    private static final String CONTINUE = "продолжить";
+    private final String path;
+    private final String botAnswers;
+
+    public ConsoleChat(String path, String botAnswers) {
+        this.path = path;
+        this.botAnswers = botAnswers;
+    }
+
+    public void run() {
+        Random random = new Random();
+        List<String> phrases = readPhrases();
+        List<String> logs = new ArrayList<>();
+        String current;
+        String answer = "Dialog start";
+        Scanner scanner = new Scanner(System.in);
+        do {
+            logs.add(answer);
+            current = scanner.nextLine();
+            logs.add(String.format("%s %s", "User:", current));
+            if (current.equals(STOP)) {
+                while (!current.equals(CONTINUE)) {
+                    current = scanner.nextLine();
+                    logs.add(String.format("%s %s", "User:", current));
+                    if (current.equals(OUT)) {
+                        return;
+                    }
+                }
+            }
+            answer = String.format("%s %s", "Console:", phrases.get(random.nextInt(phrases.size() - 1)));
+        } while (!current.equals(OUT));
+        saveLog(logs);
+    }
+
+    private List<String> readPhrases() {
+        List<String> phrases = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(botAnswers))) {
+            phrases = in.lines().collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return phrases;
+    }
+
+    private void saveLog(List<String> log) {
+        try (BufferedWriter out = new BufferedWriter(new FileWriter(path, StandardCharsets.UTF_8))) {
+            log.forEach(str -> {
+                try {
+                    out.write(str + System.lineSeparator());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean checkPath(String path) {
+        if (!Files.exists(Paths.get(path))) {
+            throw new IllegalArgumentException("File with bot answers does not exist");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ConsoleChat cc = new ConsoleChat("B:/log.txt", "B:/answers.txt");
+        checkPath(cc.botAnswers);
+        cc.run();
+    }
+}
