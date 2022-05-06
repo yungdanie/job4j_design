9906161ba62528dc9baa0a72261b2fc44d17ebeb
yungdanie <nikoches253@gmail.com>
2022-05-06 15:55:07 +0300

    123

--- src/main/java/ru/job4j/serialization/json/SimpleObject.java
+++ src/main/java/ru/job4j/serialization/json/SimpleObject.java
@@ -4,14 +4,6 @@ public class SimpleObject {
 
     private String str;
 
-    public String getStr() {
-        return str;
-    }
-
-    public void setStr(String str) {
-        this.str = str;
-    }
-
     public SimpleObject() {
     }
 
