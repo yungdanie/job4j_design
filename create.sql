6b43f3c733eeefca9dedf12d67a0c7f9cded47ad
yungdanie <nikoches253@gmail.com>
2022-05-16 16:10:31 +0300

    0. Что такое база данных. [#6860 #301573]

--- create.sql
+++ create.sql
@@ -7,4 +7,6 @@ create table cats (
 insert into cats (name, age, birthday) values ('пуся', 17, '1998-01-08'),
 									('котя', 5, '1995-03-13'),
 									('мотя', 10, '2001-10-25');
+update cats set name = 'персик' WHERE (cats.age = 17);
+delete from cats;
 select * from cats;
