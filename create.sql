a934416486747ab1aa223099aa04720e19f45a77
yungdanie <nikoches253@gmail.com>
2022-05-16 18:30:18 +0300

    1. Схема прав пользователи и роли. [#1093 #301614]

--- create.sql
+++ create.sql
@@ -1,12 +1,22 @@
-create table users(
+create table category(
 	id serial PRIMARY KEY,
-	name text,
-	role_id references roles(id)
+	category text
+);
+
+create table state(
+	id serial PRIMARY KEY,
+	state text
 );
 
 create table roles(
 	id serial PRIMARY KEY,
 	role_name text
+);
+
+create table users(
+	id serial PRIMARY KEY,
+	name text,
+	role_id int references roles(id)
 );
 
 create table rules(
@@ -16,36 +26,26 @@ create table rules(
 
 create table role_rules(
 	id serial PRIMARY KEY,
-	role_id references roles(id),
-	rule_id references rules(id)
+	role_id int references roles(id),
+	rule_id int references rules(id)
 );
 
 
 create table item(
 	id serial PRIMARY KEY,
-	user_id references users(id),
-	category_id references category(id)
-	state_id references state(id)
+	user_id int references users(id),
+	category_id int references category(id),
+	state_id int references state(id)
 );
 
 create table comments(
 	id serial PRIMARY KEY,
 	comment text,
-	item_id references item(id)
+	item_id int references item(id)
 );
 
 create table attachs(
 	id serial PRIMARY KEY,
 	attachs text,
-	item_id references item(id)
-);
-
-create table category(
-	id serial PRIMARY KEY,
-	category text
-);
-
-create table state(
-	id serial PRIMARY KEY,
-	state text
+	item_id int references item(id)
 );
