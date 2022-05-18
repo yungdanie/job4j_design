49de953a3feefa7419a0061dfe437416e346425d
yungdanie <nikoches253@gmail.com>
2022-05-18 13:32:02 +0300

    2. Группировка и агрегатные функции [#373618 #302024]

--- inititalization.sql
+++ inititalization.sql
@@ -3,15 +3,20 @@ insert into people(name) values ('Игорь'), ('Махмед'), ('Абдуль
 insert into devices_people(people_id, device_id) values (1, 1), (1, 2), (1, 3), (1, 4),
 														(2, 1), (2, 4),
 														(3, 3), (3, 4);
+
 select avg(price) from devices;
 
-select dp.name, avg(d.price) from (select device_id, people.name from devices_people as dp join people on dp.people_id = people.id) as dp
+select p.name, avg(d.price) from devices_people as dp
 join devices as d
-on dp.device_id = d.id
-group by dp.name;
+on device_id = d.id
+join people as p
+on people_id = p.id
+group by p.name;
 
-select dp.name, avg(d.price) from (select device_id, people.name from devices_people as dp join people on dp.people_id = people.id) as dp
+select p.name, avg(d.price) from devices_people as dp
 join devices as d
-on dp.device_id = d.id
-group by dp.name
+on device_id = d.id
+join people as p
+on people_id = p.id
+group by p.name
 having avg(d.price) > 5000;
