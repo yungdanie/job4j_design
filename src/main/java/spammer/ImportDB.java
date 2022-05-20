8ab97a23836cab46253ad30a4860fd1107a6ac28
yungdanie <nikoches253@gmail.com>
2022-05-20 15:15:12 +0300

    0.2. PrepareStatement [#379307 #302785]

--- src/main/java/spammer/ImportDB.java
+++ src/main/java/spammer/ImportDB.java
@@ -1,7 +1,5 @@
 package spammer;
 
-import ru.job4j.jdbc.Config;
-
 import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,16 +7,27 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Properties;
+
 
 public class ImportDB {
 
-    private final Config config = Config.getConfig("import.properties");
+    private final Properties properties;
     private final String dump;
 
     public ImportDB() {
-        this.dump = config.get("path_to_dump");
+        this.properties = getProp();
+        this.dump = properties.getProperty("path_to_dump");
+    }
+
+    private static Properties getProp() {
+        Properties prop = new Properties();
+        try (InputStream in = ImportDB.class.getClassLoader().getResourceAsStream("import.properties")) {
+            prop.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return prop;
     }
 
     public void importDB() throws IOException, ClassNotFoundException, SQLException {
@@ -27,35 +36,28 @@ public class ImportDB {
 
     private List<User> load() throws IOException {
         List<User> users = new ArrayList<>();
-        List<String[]> sepList;
         try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
-            sepList = separator(rd.lines());
-        }
-        for (String[] strings : sepList) {
-            users.add(new User(strings[0], strings[1]));
+            rd.lines().forEach(line -> {
+                String[] array = line.split(";", 2);
+                if (array.length != 2 || array[0].isEmpty() || array[1].isEmpty()) {
+                    throw new IllegalArgumentException();
+                }
+                users.add(new User(array[0], array[1]));
+            });
         }
         return users;
     }
 
-    private static List<String[]> separator(Stream<String> stream) {
-        List<String> list = stream.collect(Collectors.toList());
-        List<String[]> sepList = new ArrayList<>();
-        for (String str : list) {
-            sepList.add(str.split(";"));
-        }
-        return sepList;
-    }
-
     private void save(List<User> users) throws ClassNotFoundException, SQLException {
-        Class.forName(config.get("jdbc.driver"));
+        Class.forName(properties.getProperty("jdbc.driver"));
         try (Connection cnt = DriverManager.getConnection(
-                config.get("jdbc.url"),
-                config.get("jdbc.username"),
-                config.get("jdbc.password")
+                properties.getProperty("jdbc.url"),
+                properties.getProperty("jdbc.username"),
+                properties.getProperty("jdbc.password")
         )) {
             for (User user : users) {
                 try (PreparedStatement ps = cnt.prepareStatement(String.format("insert into %s(name, email) values (?, ?)",
-                        config.get("table_name")))) {
+                        properties.getProperty("table_name")))) {
                     ps.setString(1, user.name);
                     ps.setString(2, user.email);
                     ps.execute();
